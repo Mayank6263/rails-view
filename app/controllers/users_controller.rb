b63a08b254da0408989be032b5c56de46ff1e7b6
Office's Ubuntu/linux <75mayanksolanki.com>
2025-10-06 19:44:34 +0530

    Controller and view  started

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,17 +4,46 @@ class UsersController < ApplicationController
   end
 
   def new
+    #only for get request
     @user = User.new
   end
 
   def create
+    #post request is going to execute here
+    @user = User.new(user_params)
     if @user.save
+      flash[:notice] = " #{@user.name} Successfully Created"
+      render :index
+    else
+      flash.now[:notice] = "Error in Creation"
+      render :new
+    end
   end
 
   def show
+    @user = User.find(params[:id])
+    # render plain: "name :- #{user[:name]}, 
+    # \nusername :- #{user[:username]}, 
+    # \nE-mail :- #{user[:email]},"
+    # \nparams? #{params}"
+    # render plain: "params #{params[:user]}"
+  end
+
+  def edit
+    #only for get request 
+    @user = User.find(params[:id])
   end
 
   def update
+    #put and patch request is going to execute here
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = " # Successfully Updated"
+      render :index
+    else
+      flash[:notice] = "Failed To update"
+      render :edit
+    end
   end
 
   def delete
@@ -22,4 +51,6 @@ class UsersController < ApplicationController
 
   private
   def user_params
+    params.require(:user).permit(:username, :name, :email, :password)
+  end
 end
